e642d846ba37f117c0919140bdd20760d0abaa71
Kevdl01 <vivek.vc04@gmail.com>
2021-07-11 18:46:12 -0400

    1

--- src/init.cpp
+++ src/init.cpp
@@ -531,8 +531,8 @@ std::string HelpMessage(HelpMessageMode mode)
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/litecoin-project/litecoin>";
-    const std::string URL_WEBSITE = "<https://litecoin.org>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/bluecoin-project/bluecoin>";
+    const std::string URL_WEBSITE = "<https://bluecoin.org>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2011, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
